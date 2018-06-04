a8ac71e6fc97811cfb49d343fb9f96192453ef07
bonowg <bonowg@gmail.com>
2018-06-04 21:53:13 +0200

    if

---
+++ Untitled22.ps1
@@ -0,0 +1,8 @@
+﻿$baseDir = 'C:\Users\bonow\Desktop\PSE\powershell\'
+$filename = 'FileToCopy.txt'
+Get-Process | Out-File -FilePath "$baseDir$filename"
+$netShare = '\\server01\C$\temp\NewFile.txt'
+Copy-Item -Path "$baseDir$filename" -Destination $netShare
+try { Copy-Item -Path "$baseDir$filename" -Destination $netShare -ErrorAction SilentlyContinue }
+catch { "file cannot be copied"
+$Error[0].Exception.Message }
---
+++ Untitled23.ps1
@@ -0,0 +1,28 @@
+﻿Get-WmiObject -Class win32_logicaldisk -Filter "DeviceID='C:'"
+$driveLetter = 'c:'
+Get-WmiObject -Class win32_logicaldisk -Filter "DeviceID='$driveLetter'" | Get-Member
+$object = Get-WmiObject -Class win32_logicaldisk -Filter "DeviceID='$driveLetter'"
+$object.VolumeSerialNumber
+Get-WmiObject -Class win32_logicaldisk -Filter "DeviceID='$driveLetter'" | Select-Object Size, FreeSpace, @{ name="Used"; expression={$PSItem.Size-$PSItem.FreeSpace} }
+[string]$valueToReturn = 'FreeSpace'
+if ( $valueToReturn -eq 'Size' )
+{ Get-WmiObject -Class win32_logicaldisk -Filter "DeviceID='$driveLetter'" | Select-Object Size }
+ElseIf ($valueToReturn -eq 'FreeSpace' )
+{ Get-WmiObject -Class win32_logicaldisk -Filter "DeviceID='$driveLetter'" | Select-Object FreeSpace }
+Else
+{ Get-WmiObject -Class win32_logicaldisk -Filter "DeviceID='$driveLetter'" | Select-Object @{ name="Used"; expression={$PSItem.Size-$PSItem.FreeSpace} } }
+
+
+Test-Connection -ComputerName 'DESKTOP-VQ7I9CG'
+Test-Connection -Quiet -ComputerName 'DESKTOP-VQ7I9CG'
+
+If (Test-Connection -Quiet -ComputerName 'DESKTOP-VQ7I9CG')
+{
+Write-Host "Connection OK"
+}
+else
+{
+Write-Host "Cannot connect"
+}
+
+
