521759442b7b0ebbd3e59e31f1f09f6597f18400
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-06-06 15:16:38 +0200

    reszta

---
+++ Untitled36.ps1
@@ -0,0 +1,14 @@
+﻿cd alias:
+ls
+cd variable:
+ls
+Get-PSProvider
+Get-Module -ListAvailable
+Get-PSDrive
+New-PSDrive TestyPS FileSystem C:\repos\powershell
+Get-Alias 
+Get-PSDrive
+Get-Command -Module NetTCPIP
+Get-NetIPAddress | Where-Object { $PSItem.PrefixOrigin -eq 'DHCP' }
+Get-Module 
+Remove-Module NetTCPIP
