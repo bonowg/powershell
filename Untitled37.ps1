7914565d048a6e8d153abef03ffdc06fcbbe59cf
bonowg <bonowg@gmail.com>
2018-06-08 22:52:31 +0200

    remoting2

--- Untitled37.ps1
+++ Untitled37.ps1
@@ -1,9 +1,11 @@
 ﻿$asus = 'DESKTOP-6S6V7RL'
 $cred = Get-Credential
 $cred
-Enter-PSSession -ComputerName $asus -Credential $cred -Name Nowa
+Enter-PSSession -ComputerName $asus -Credential $cred
 Get-PSSession
 Invoke-Command -ComputerName $asus -Credential $cred -ScriptBlock { Get-Process }
 Invoke-Command -ComputerName $asus -Credential $cred -ScriptBlock { Get-WmiObject -Class win32_operatingsystem } | Get-Member
+Invoke-Command -ComputerName $asus -Credential $cred -ScriptBlock { Get-Process -Name 'notepad' }
+Invoke-Command -ComputerName $asus -Credential $cred -ScriptBlock { Get-Process -Name 'notepad' | Stop-Process }
 Get-Item WSMan:\localhost\Client\TrustedHosts
 Invoke-Command -ComputerName $asus -Credential $cred -ScriptBlock { get-content Env:\COMPUTERNAME }
